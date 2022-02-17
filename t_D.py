19cfb790661492d30115707331616335ae450a70
master-cim <master-cim@yandex.ru>
2022-02-17 22:03:24 +0300

    t_D правильно но доделать

--- t_D.py
+++ t_D.py
@@ -4,17 +4,12 @@ from typing import List
 def get_weather_randomness(n: int, temperatures: List[int]) -> int:
     result = 0
     if n != 1:
-        day_prev = [temperatures[i]-temperatures[i-1] for i in range(
-            1, len(temperatures))]
         print(temperatures)
-        print(day_prev)
-        # if day_prev[n-2] < day_prev[n-3] or day_prev[0] > day_prev[1]:
-        #     result = 1
-        for c in range(0, len(day_prev)-1):
-            if (day_prev[c] > 1 and
-                day_prev[c] > day_prev[c-1] and
-                day_prev[c] > day_prev[c+1]):
-                print(day_prev[c])
+        for c in range(1, n-1):
+            if ((temperatures[c] > temperatures[c+1] and
+                temperatures[c] > temperatures[c-1]) or
+                (temperatures[c] < temperatures[c+1] and
+                 temperatures[c] < temperatures[c-1])):
                 result += 1
         return result
     else:
