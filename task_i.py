059f6924cd81b4af8a2a51c6d7ac9a4fd4393416
master-cim <master-cim@yandex.ru>
2022-02-18 21:50:17 +0300

    решила задачу I

---
+++ task_i.py
@@ -0,0 +1,11 @@
+import math
+
+
+def is_power_of_four(number: int) -> bool:
+    log = math.log(number, 4)
+    if int(log * 100) % 100 == 0:
+        return 'True'
+    else:
+        return 'False'
+
+print(is_power_of_four(int(input())))
