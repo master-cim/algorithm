66b0b1512d60472902c173af57c8ab06ddda9ac0
master_cim <master-cim@yandex.ru>
2022-02-18 13:11:03 +0300

    задача Е сделана

--- task_e.py
+++ task_e.py
@@ -1,14 +1,18 @@
 def get_longest_word(line: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    word = line.split()
+    max_string = max(word, key=len)
+    return(max_string)
+
 
 def read_input() -> str:
     _ = input()
     line = input().strip()
     return line
 
+
 def print_result(result: str) -> None:
     print(result)
     print(len(result))
+
 
 print_result(get_longest_word(read_input()))
