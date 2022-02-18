c68bcf2b2ae6f48a9aa8a6080ca2aa446f57ebd3
master-cim <master-cim@yandex.ru>
2022-02-18 22:10:34 +0300

    факториал

---
+++ task_j.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def factorize(number: int) -> List[int]:
+    i = 2
+    result = []
+    while i * i <= number:
+        while number % i == 0:
+            result.append(i)
+            number = int(number / i)
+        i = i + 1
+    if number > 1:
+        result.append(number)
+    return result
+
+
+result = factorize(int(input()))
+print(" ".join(map(str, result)))
