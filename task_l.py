5aa70eebc41d30e4fa5b25f13d5b97a4ce5ef1ae
master-cim <master-cim@yandex.ru>
2022-02-18 23:49:41 +0300

    сделала Л

--- task_l.py
+++ task_l.py
@@ -1,13 +1,21 @@
 from typing import Tuple
 
+
 def get_excessive_letter(shorter: str, longer: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    len_list = len(longer)
+    len_sh = shorter.ljust(len_list, "~")
+    sorted_shorter = sorted(len_sh)
+    sorted_longer = sorted(longer)
+    for i in range(0, len_list):
+        if sorted_shorter[i] != sorted_longer[i]:
+            return sorted_longer[i]
+
 
 def read_input() -> Tuple[str, str]:
     shorter = input().strip()
     longer = input().strip()
     return shorter, longer
+
 
 shorter, longer = read_input()
 print(get_excessive_letter(shorter, longer))
