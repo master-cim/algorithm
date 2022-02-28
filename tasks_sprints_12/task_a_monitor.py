ac93cf1743080318ca4cdeec782005958cc7458f
master-cim <master-cim@yandex.ru>
2022-02-28 21:27:07 +0300

    Решена задача Транспорент

--- tasks_sprints_12/task_a_monitor.py
+++ tasks_sprints_12/task_a_monitor.py
@@ -1,34 +1,33 @@
 # A. Мониторинг
-# ID успешной посылки 
+# ID успешной посылки
 
 from typing import List, Tuple
 
 
 def transporate(n: int, m: int, matrix: List[int]) -> List[int]:
-    result = []
-    idx_n = 0
     idx_m = 0
     while idx_m < m:
+        result = []
+        idx_n = 0
         while idx_n < n:
-            result.append(list(matrix[idx_n][idx_m]))
+            result.append(matrix[idx_n][idx_m])
             idx_n += 1
-            
-            print(result)
-        # for item in range(len(matrix)):
-        #     result.append(list(map(int, input().strip().split())))
         idx_m += 1
-    return(result)
+        print_array(result)
 
 
 def read_input() -> Tuple[int, int, List[List[int]]]:
     n = int(input())
     m = int(input())
     matrix = []
-    for i in range(n):
+    for _ in range(n):
         matrix.append(list(map(int, input().strip().split())))
-    print(matrix)
     return n, m, matrix
 
 
+def print_array(result: List[int]):
+    print(" ".join(map(str, result)))
+
+
 n, m, matrix = read_input()
-print(" ".join(map(str, transporate(n, m, matrix))))
+transporate(n, m, matrix)
