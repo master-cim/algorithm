034f2da3331755df5b53e0897eed91336a913d46
master-cim <master-cim@yandex.ru>
2022-03-01 22:21:00 +0300

    D сделано

--- tasks_sprints_12/d_caring_mother.py
+++ tasks_sprints_12/d_caring_mother.py
@@ -1,18 +1,32 @@
-# Comment it before submitting
-# class Node:  
-#     def __init__(self, value, next_item=None):  
-#         self.value = value  
-#         self.next_item = next_item
+# D. Заботливая мама
+# ID успешной посылки 65663041
+
+class Node:  
+    def __init__(self, value, next_item=None):
+        self.value = value
+        self.next_item = next_item
+
 
 def solution(node, elem):
-    # Your code
-    # ヽ(´▽`)/
-    pass
+    count = 0
+    while node.value != elem:
+        if node.value != elem and node.next_item is None:
+            count = -1
+            break
+        else:
+            count = count + 1
+            node = node.next_item
+    return count
+
 
 def test():
     node3 = Node("node3", None)
     node2 = Node("node2", node3)
     node1 = Node("node1", node2)
     node0 = Node("node0", node1)
-    idx = solution(node0, "node2")
+    solution(node0, "node4")
     # result is idx == 2
+
+
+if __name__ == '__main__':
+    test()
