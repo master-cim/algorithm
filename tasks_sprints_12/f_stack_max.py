4086f00ae4b1533949f153f210c3127b7a31e9ad
master_cim <master-cim@yandex.ru>
2022-03-02 14:55:02 +0300

    F. Стек - Max

---
+++ tasks_sprints_12/f_stack_max.py
@@ -0,0 +1,55 @@
+# F. Стек - Max
+# ID успешной посылки
+
+from typing import List, Tuple
+
+
+class StackMax:
+    def __init__(self):
+        self._items = []
+
+    def push(self, item: int):
+        self._items.append(item)
+
+    def pop(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            print('error')
+
+    def get_max(self):
+        if self._items != []:
+            return max(self._items)
+        else:
+            return('None')
+
+    def __len__(self):
+        return len(self._items)
+
+    def __repr__(self):
+        return f'{self._items}'
+
+
+def run_stack(command_list: List[List[str]]):
+    stack = StackMax()
+    for command in range(0, len(command_list)):
+        if command_list[command][0] == 'push':
+            stack.push(command_list[command][1])
+        elif command_list[command][0] == 'pop':
+            stack.pop()
+        elif command_list[command][0] == 'get_max':
+            max_value = stack.get_max()
+            print(max_value)
+
+
+def read_input() -> Tuple[List[List[str]]]:
+    n = int(input())
+    command_list = []
+    for _ in range(n):
+        command_list.append(list(map(str, input().strip().split())))
+    return(command_list)
+
+
+if __name__ == '__main__':
+    command_list = read_input()
+    run_stack(command_list)
