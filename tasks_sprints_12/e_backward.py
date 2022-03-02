5cbd292c0004f71bb2a2b122491a9a3b0b6e56f3
master_cim <master-cim@yandex.ru>
2022-03-02 10:31:37 +0300

    задача Е

---
+++ tasks_sprints_12/e_backward.py
@@ -0,0 +1,52 @@
+# E. Всё наоборот
+
+
+class DoubleConnectedNode:
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+
+def print_linked_list(node):
+    while node:
+        print(node.value, end="\n")
+        node = node.prev
+
+
+def solution(node):
+    prev = None
+    while node is not None:
+        next = node.next
+        node.prev = prev
+        prev = node
+        node = next
+    new_head = prev
+    print_linked_list(new_head)
+
+
+def test():
+    node3 = DoubleConnectedNode("node3")
+    node2 = DoubleConnectedNode("node2")
+    node1 = DoubleConnectedNode("node1")
+    node0 = DoubleConnectedNode("node0")
+
+    node0.next = node1
+
+    node1.prev = node0
+    node1.next = node2
+
+    node2.prev = node1
+    node2.next = node3
+
+    node3.prev = node2
+    solution(node0)
+    # result is new_head == node3
+    # node3.next == node2
+    # node2.next == node1 node2.prev == node3
+    # node1.next == node0 node1.prev == node2
+    # node0.prev == node1
+
+
+if __name__ == '__main__':
+    test()
