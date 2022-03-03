1ec290d435de73b8e90e763b3c03955f7d971c67
master_cim <master-cim@yandex.ru>
2022-03-03 10:41:14 +0300

    H. Скобочная последовательность частичное решение

--- tasks_sprints_12/h_brack_sequence.py
+++ tasks_sprints_12/h_brack_sequence.py
@@ -1,5 +1,6 @@
 # H. Скобочная последовательность
 # ID успешной посылки
+# не проходит вариант (){}[] и ({})({[]}[])
 from typing import List, Tuple
 
 
@@ -10,9 +11,21 @@ def is_correct_bracket_seq(bracket_list):
     if members % 2 != 0:
         return False
     midle = int(members / 2)
-    while i < midle:
-        if bracket_list[i] members
-        i += 1
+    idx_forward = 0
+    idx_back = -1
+    count = 0
+    while (idx_forward < midle) and (
+        (bracket_list[idx_forward] + bracket_list[idx_back]) == '[]'
+        or (bracket_list[idx_forward] + bracket_list[idx_back]) == '{}'
+        or (bracket_list[idx_forward] + bracket_list[idx_back]) == '()'
+    ):
+        idx_forward += 1
+        idx_back -= 1
+        count += 1
+    if count == midle:
+        return True
+    else:
+        return False
 
 
 def read_input() -> Tuple[List[List[str]]]:
