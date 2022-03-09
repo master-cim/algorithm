939e92b12027afe247d1bd8ea378e14f7a23e8e4
master_cim <master-cim@yandex.ru>
2022-03-09 10:55:37 +0300

    калькулятор

--- tasks_sprints_12/final/B_calculator.py
+++ tasks_sprints_12/final/B_calculator.py
@@ -1,79 +1,29 @@
 # B. Калькулятор
 # ID успешной посылки
 
-from typing import List, Tuple
+from typing import Tuple
 
 
-class QueueOperands:
-    def __init__(self):
-        self.items = []
-        self.top = -1
-
-    def push(self, item: int):
-        if self.isFull():
-            print('error')
+def calculator(put_items: Tuple[str]):
+    stack_items = []
+    # top = -1
+    list_operations = ('-', '+', '*', '/')
+    for step in range(0, len(put_items)):
+        if put_items[step] not in list_operations:
+            stack_items.append(put_items[step])
         else:
-            self.top += 1
-            self.items.append(int(item))
-
-    def pop(self):
-        try:
-            if self.isEmpty():
-                return('None')
-            else:
-                self.top -= 1
-                return self.items.pop(0)
-        except IndexError:
-            return('error')
-
-    def peek(self):
-        if self.top == -1:
-            return('None')
-        else:
-            return(self.items[0])
-
-    def isEmpty(self):
-        if self.top == -1:
-            return True
-        else:
-            return False
-
-    def size(self):
-        return len(self.items)
+            x = int(stack_items[step-2]) * int(stack_items[step-1])
+            stack_items.append(str(x))
+            print(x)
+        print(stack_items)
 
 
-def calculator():
-    items = []
-    top = -1
 
-
-# def run_stack(quantum_command: int,
-#               queue_length: int, command_list: List[List[str]]):
-#     max_size = queue_length
-#     stack = MyQueueSized(max_size)
-#     for command in range(0, quantum_command):
-#         if command_list[command][0] == 'push':
-#             stack.push(command_list[command][1])
-#         elif command_list[command][0] == 'pop':
-#             pop_value = stack.pop()
-#             print(pop_value)
-#         elif command_list[command][0] == 'peek':
-#             peek_value = stack.peek()
-#             print(peek_value)
-#         elif command_list[command][0] == 'size':
-#             len_queue = stack.size()
-#             print(len_queue)
-
-
-def read_input() -> Tuple[List[List[str]]]:
-    quantum_command = int(input())
-    queue_length = int(input())
-    command_list = []
-    for _ in range(quantum_command):
-        command_list.append(list(map(str, input().strip().split())))
-    return(quantum_command, queue_length, command_list)
+def read_input() -> Tuple[str]:
+    put_items = tuple(map(str, input().strip().split()))
+    return put_items
 
 
 if __name__ == '__main__':
-    quantum_command, queue_length, command_list = read_input()
-    run_stack(quantum_command, queue_length, command_list)
+    put_items = read_input()
+    calculator(put_items)
