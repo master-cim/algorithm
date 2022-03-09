4afdeb7c4c261520d341e7f0d0c470fc1189d598
master_cim <master-cim@yandex.ru>
2022-03-09 15:37:16 +0300

    первая версия

--- tasks_sprints_12/final/B_calculator.py
+++ tasks_sprints_12/final/B_calculator.py
@@ -1,22 +1,54 @@
 # B. Калькулятор
 # ID успешной посылки
-
+from __future__ import division
 from typing import Tuple
+
+
+class MyCalculate:
+    def __init__(self, val_x, val_y):
+        self.value_x = val_x
+        self.value_y = val_y
+
+    def multiplication(self, value_x, value_y):
+        return int(value_x) * int(value_y)
+
+    def devision(self, value_x, value_y):
+        result = float(value_x) / float(value_y)
+        if result <= 0:
+            return round(result)
+        return result
+
+    def subtraction(self, value_x, value_y):
+        return int(value_x) - int(value_y)
+
+    def addition(self, value_x, value_y):
+        return int(value_x) + int(value_y)
 
 
 def calculator(put_items: Tuple[str]):
     stack_items = []
+    stack_opr = []
     # top = -1
     list_operations = ('-', '+', '*', '/')
     for step in range(0, len(put_items)):
         if put_items[step] not in list_operations:
-            stack_items.append(put_items[step])
+            stack_items.append(float(put_items[step]))
+            print(stack_items)
         else:
-            x = int(stack_items[step-2]) * int(stack_items[step-1])
-            stack_items.append(str(x))
-            print(x)
-        print(stack_items)
-
+            stack_opr.append(put_items[step])
+            print(stack_opr)
+            # stack_len = len(stack_items)
+            x = stack_items.pop(-2)
+            y = stack_items.pop(-1)
+            o = stack_opr.pop()
+            print ( x, o, y)
+            expr = MyCalculate(x, y)
+            dic_match_fn_to_opr = {'-': expr.subtraction(x, y),
+                                   '+': expr.addition(x, y),
+                                   '*': expr.multiplication(x, y),
+                                   '/': expr.devision(x, y)}
+            stack_items.append(dic_match_fn_to_opr.get(o))
+        print(int(stack_items[-1]))
 
 
 def read_input() -> Tuple[str]:
