c2faf4960468e6aad12f86b8be2d4c5e95482c45
master-cim <master-cim@yandex.ru>
2022-03-10 22:00:46 +0300

    задача калькулятор с помощью стека

--- tasks_sprints_12/final/B_calculator_test_v2.py
+++ tasks_sprints_12/final/B_calculator_test_v2.py
@@ -1,28 +1,45 @@
 # B. Калькулятор
-# ID успешной посылки 65886740
+# ID успешной посылки "список" 65886740
+# ID успешной посылки "класс Стек"  65920846
 from typing import Tuple
 import operator
 
 OPERATORS = {
-    '-': operator.sub,
-    '+': operator.add,
-    '*': operator.mul,
-    '/': operator.floordiv
+    '-': 'sub',
+    '+': 'add',
+    '*': 'mul',
+    '/': 'floordiv'
 }
 
 
+class StackOperands:
+    def __init__(self):
+        self.__items = []
+
+    def push(self, item: int):
+        self.__items.append(item)
+
+    def pop(self):
+        try:
+            return self.__items.pop()
+        except IndexError:
+            print('error')
+
+    def __repr__(self):
+        return f'{self.__items}'
+
+
 def calculator(put_items: Tuple[str]):
-    stack_items = []
-    list_operations = ('-', '+', '*', '/')
+    stack_items = StackOperands()
     for step in range(0, len(put_items)):
-        if put_items[step] not in list_operations:
-            stack_items.append(put_items[step])
+        if put_items[step] in OPERATORS:
+            method = getattr(operator, OPERATORS.get(put_items[step]))
+            first_out_one = stack_items.pop()
+            first_out_two = stack_items.pop()
+            stack_items.push(method(first_out_two, first_out_one))
         else:
-            x = stack_items.pop(-2)
-            y = stack_items.pop(-1)
-            operator = OPERATORS[put_items[step]]
-            stack_items.append(operator(float(x), float(y)))
-    return(int(stack_items[-1]))
+            stack_items.push(int(put_items[step]))
+    return stack_items.pop()
 
 
 def read_input() -> Tuple[str]:
