0da1ff24f9be3a382d859e755d6719486b4454e4
master-cim <master-cim@yandex.ru>
2022-03-15 22:11:10 +0300

    ищет первый элемент соответствующий условию

--- recurce_13/l_two_bikes.py
+++ recurce_13/l_two_bikes.py
@@ -1,17 +1,28 @@
 # L. Два велосипеда
-# ID успешной посылки
+# ID успешной посылки https://tirinox.ru/find-first-python/
 
 
 def amount_for_bike(cash_per_day, cost_bike, left, right):
-    if right <= left:
-        return -1
-    mid = (left + right) // 2
-    if cash_per_day[mid] == cost_bike:
-        return mid
-    elif cost_bike < cash_per_day[mid]:
-        return amount_for_bike(cash_per_day, cost_bike, left, mid+1)
-    else:
-        return amount_for_bike(cash_per_day, cost_bike, mid+1, right)
+    # if right <= left:
+    #     return -1
+    # for x in cash_per_day:
+    #     if x >= cost_bike:
+    #         print(cash_per_day.index(x)+1)
+    #         break
+    #     else:
+    #         print(-1)
+    result = [cash_per_day.index(x)+1 for x in cash_per_day if x >= cost_bike][0]
+    # result = next(cash_per_day.index(x) for x in cash_per_day if cash_per_day[x] >= cost_bike)
+    return result
+    
+    # mid = (left + right) // 2
+    # if cash_per_day[mid] == cost_bike:
+        
+    #     return result
+    # elif cost_bike < cash_per_day[mid]:
+    #     return amount_for_bike(cash_per_day, cost_bike, left, mid+1)
+    # else:
+    #     return amount_for_bike(cash_per_day, cost_bike, mid+1, right)
 
 
 def read_input():
@@ -25,6 +36,4 @@ if __name__ == '__main__':
     number_days, cash_per_day, cost_bike = read_input()
     print(amount_for_bike(
         cash_per_day, cost_bike, left=0, right=number_days)
-    )
-          
-          
+    ) 
