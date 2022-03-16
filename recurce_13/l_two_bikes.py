74ce33b3d83c42b707c4b2dc28f4f77609958dc6
master_cim <master-cim@yandex.ru>
2022-03-16 10:35:06 +0300

    время

--- recurce_13/l_two_bikes.py
+++ recurce_13/l_two_bikes.py
@@ -1,39 +1,36 @@
 # L. Два велосипеда
-# ID успешной посылки https://tirinox.ru/find-first-python/
+# ID успешной посылки 
 
 
 def amount_for_bike(cash_per_day, cost_bike, left, right):
     if right <= left:
         return -1
-    # result = [cash_per_day.index(x)+1 for x in cash_per_day if x >= cost_bike][0]
-    # result = next((cash_per_day.index(x)+1 for x in cash_per_day if x >= cost_bike), '-1')
-    # return result
     mid = (left + right) // 2
-    if cost_bike <= cash_per_day[mid]:
-
-        # result = next((cash_per_day.index(x)+1 for x in cash_per_day if x >= cost_bike), '-1')
-        return amount_for_bike(cash_per_day, cost_bike, left, mid)
+    if cash_per_day[mid] >= cost_bike:
+        mid = next(
+            (cash_per_day.index(x)+1 for x in cash_per_day if x >= cost_bike),
+            '-1')
+        return mid
+    elif cash_per_day[mid] > cost_bike:
+        result = amount_for_bike(cash_per_day, cost_bike, left, mid-1)
+        return result
     else:
-        return amount_for_bike(cash_per_day, cost_bike, mid+1, right)
-    
-    # mid = (left + right) // 2
-    # if cash_per_day[mid] == cost_bike:
-    #     return mid
-    # elif cost_bike < cash_per_day[mid]:
-    #     return amount_for_bike(cash_per_day, cost_bike, left, mid)
-    # else:
-    #     return amount_for_bike(cash_per_day, cost_bike, mid+1, right)
+        result = amount_for_bike(cash_per_day, cost_bike, mid+1, right)
+        return result
 
 
 def read_input():
     number_days = int(input())
     cash_per_day = [int(element) for element in input().strip().split()]
     cost_bike = int(input())
-    return(number_days, cash_per_day, cost_bike)
+    two_bikes = cost_bike * 2
+    return(number_days, cash_per_day, cost_bike, two_bikes)
 
 
 if __name__ == '__main__':
-    number_days, cash_per_day, cost_bike = read_input()
+    number_days, cash_per_day, cost_bike, two_bikes = read_input()
     print(amount_for_bike(
-        cash_per_day, cost_bike, left=0, right=number_days)
-    ) 
+        cash_per_day, cost_bike, left=0, right=number_days),
+          amount_for_bike(
+        cash_per_day, two_bikes, left=0, right=number_days)
+    )
