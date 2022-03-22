e01ad774b6927085221f2921e77bb9c94ef71453
master-cim <master-cim@yandex.ru>
2022-03-22 22:38:02 +0300

    клумбы не идут

--- recurce_13/n_flowerbed_v1.py
+++ recurce_13/n_flowerbed_v1.py
@@ -5,31 +5,39 @@ from functools import reduce
 
 def comparison_f(left, right):
     sub_arr = []
-    if left[1] > right[0] and left[1] > right[1]:
+    if left[1] < right[0]:
         return left
-    elif left[1] <= right[1] and left[1] > right[0]:
+    elif (left[1] >= right[0] and left[1] < right[1]):
         sub_arr.append(left[0])
         sub_arr.append(right[1])
         return(sub_arr)
-    elif left[1] < right[0]:
-        return left
+    # elif (left[1] > right[0] and left[1] <= right[1]):
+    #     sub_arr.append(left[0])
+    #     sub_arr.append(right[1])
+    #     return(sub_arr)
 
 
 def comparison(left, right):
     sub_arr = []
-    if left[1] > right[0] and left[1] > right[1]:
-        return left
-    elif left[1] <= right[1] and left[1] > right[0]:
+    if left[1] < right[0]: 
+        return right
+    elif (left[1] >= right[0] and left[1] < right[1]):
         sub_arr.append(left[0])
         sub_arr.append(right[1])
         return(sub_arr)
-    elif left[1] < right[0]:
-        return right
+    else:
+        return
+    # elif (left[1] > right[0] and left[1] <= right[1]):
+    #     sub_arr.append(left[0])
+    #     sub_arr.append(right[1])
+    #     return(sub_arr)
 
 
 def flowerbed(arr):
     result = []
-    for i in range(1, len(arr)):
+    result.append(comparison_f(arr[0], arr[1]))
+    
+    for i in range(2, len(arr)):
         m = (comparison(arr[i-1], arr[i]))
         print(m)
         if m is not None:
@@ -37,24 +45,20 @@ def flowerbed(arr):
     return (result)
 
 
-def test(result):
-    return(flowerbed(result))
-
-
 def read_input():
     number_gardeners = int(input())
     flowerbed_coordinates = []
     for _ in range(number_gardeners):
         flowerbed_coordinates.append(list(map(int, input().strip().split())))
-    unique = reduce(lambda l, x: l+[x] if x not in l else l, flowerbed_coordinates, [])
+    unique = reduce(
+        lambda l, x: l+[x] if x not in l else l, flowerbed_coordinates, [])
     flowerbed_coordinates = sorted(unique)
-    print(flowerbed_coordinates)
+
     return flowerbed_coordinates
 
 
 if __name__ == '__main__':
     flowerbed_coordinates = read_input()
-    # print(flowerbed_coordinates)
-    # print(flowerbed(flowerbed_coordinates))
+    print(flowerbed_coordinates)
     print(flowerbed(flowerbed_coordinates))
     # print(*(list(zip(*flowerbed_coordinates))[0]), sep="\n")
