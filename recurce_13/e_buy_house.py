e959bd93d1699e3e6297c6e6093164d2e91c4d6c
master_cim <master-cim@yandex.ru>
2022-03-22 10:31:03 +0300

    E. Покупка домов ID успешной посылки 66298098

--- recurce_13/e_buy_house.py
+++ recurce_13/e_buy_house.py
@@ -1,32 +1,28 @@
 # E. Покупка домов
-# ID успешной посылки
+# ID успешной посылки 66298098
 
-def potential_shopping(price_house, cash,):
-    if cash < 0:
+def potential_shopping(price_house, cash):
+    sum_houses = 0
+    item = 0
+    if len(price_house) == 1:
         return 1
-    
-def amount_for_bike(cash_per_day, cost_bike, left, right):
-    if (right <= left and left != 0):
-        return -1
-    middle = (left + right) // 2
-    if (cash_per_day[middle] >= cost_bike and (cash_per_day[middle - 1] < cost_bike or middle == 0)):
-        return middle + 1
-    elif cost_bike <= cash_per_day[middle]:
-        return amount_for_bike(cash_per_day, cost_bike, left, middle)
+    elif len(price_house) != 0:
+        while sum_houses <= cash:
+            sum_houses += price_house[item]
+            item += 1
+        return item - 1
     else:
-        return amount_for_bike(cash_per_day, cost_bike, middle + 1, right)
+        return 0
 
 
 def read_input():
     input_list = list(map(int, input().split()))
-    num_views = input_list[0]
     cash = input_list[1]
-    price_house = sorted(list(map(int, input().split())))
-    return num_views, cash, price_house
+    price_house = list(map(int, input().split()))
+    price_house = sorted([item for item in price_house if item <= cash])
+    return cash, price_house
 
 
 if __name__ == '__main__':
-    num_views, cash, price_house = read_input()
-    print(price_house, cash, left=0, right=num_views)
-    print(amount_for_bike(
-        cash_per_day, cost_bike, left=0, right=number_days)
+    cash, price_house = read_input()
+    print(potential_shopping(price_house, cash))
